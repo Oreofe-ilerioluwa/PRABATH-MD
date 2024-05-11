7a9b474bcbec066e6de90142ee7856575deac9cb
Oreofe_ilerioluwa <93833869+Oreofe-ilerioluwa@users.noreply.github.com>
2024-05-11 14:15:52 +0100

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
-BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
-PASSWORD: process.env.PASSWORD || "Enter you like password",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~pmFDEZxC#HyWb59dvECirNAe_-krh8uQhgShDQa4NMf2X8ayMurY",
+BOT_NUMBER: process.env.BOT_NUMBER || "2349151066117",
+PASSWORD: process.env.PASSWORD || "@Adeileri#6859!",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Oreofe-ilerioluwa",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
 };
